4bd0c6161151f7f88c1768f5ec004ea7bac22c9d
haccht <haccht@users.noreply.github.com>
2023-11-03 15:33:25 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# histgram
+
+Render histograms in the terminal.
+
+
+```
+$ histgram -h
+Usage:
+  histogram [OPTIONS]
+
+Application Options:
+  -b, --bins=  Number of bins in the histogram (default: 10)
+  -c, --chart  Draw the bar chart
+
+Help Options:
+  -h, --help   Show this help message
+```
+
+`histogram` render histograms from the given list of numbers.
+
+
+```
+$ head normaldistribution.txt
+-2.17009037776025
+1.811910672953939
+-1.6953977102723776
+-1.5981931399228735
+-1.5141018876192867
+1.4395314709384586
+-1.3722038089987159
+2.575829326545648
+-1.9599639845509635
+-1.3105791121681314
+
+$ cat normaldistribution.txt | histgram
+Total count = 300
+Min/Avg/Max = -2.58 / 0.00 / 2.58
+
+[   -2.58 -    -2.06]	       6
+[   -2.06 -    -1.55]	      12
+[   -1.55 -    -1.03]	      27
+[   -1.03 -    -0.52]	      45
+[   -0.52 -    -0.00]	      60
+[    0.00 -     0.52]	      60
+[    0.52 -     1.03]	      45
+[    1.03 -     1.55]	      27
+[    1.55 -     2.06]	      12
+[    2.06 -     2.58]	       6
+```
+
+`histogram` can also draw a bar chart of the histogram..
+
+```
+$ cat normaldistribution.txt | histgram -c
+Total count = 300
+Min/Avg/Max = -2.58 / 0.00 / 2.58
+
+[   -2.58 -    -2.06]	       6  ||||
+[   -2.06 -    -1.55]	      12  ||||||||
+[   -1.55 -    -1.03]	      27  ||||||||||||||||||
+[   -1.03 -    -0.52]	      45  ||||||||||||||||||||||||||||||
+[   -0.52 -    -0.00]	      60  ||||||||||||||||||||||||||||||||||||||||
+[    0.00 -     0.52]	      60  ||||||||||||||||||||||||||||||||||||||||
+[    0.52 -     1.03]	      45  ||||||||||||||||||||||||||||||
+[    1.03 -     1.55]	      27  ||||||||||||||||||
+[    1.55 -     2.06]	      12  ||||||||
+[    2.06 -     2.58]	       6  ||||
+```
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	flags "github.com/jessevdk/go-flags"
+)
+
+type options struct {
+	Bins  int  `short:"b" long:"bins" description:"Number of bins in the histogram" default:"10"`
+	Chart bool `short:"c" long:"chart" description:"Draw the bar chart"`
+}
+
+func main() {
+	var opts options
+	args, err := flags.Parse(&opts)
+	if err != nil {
+		if fe, ok := err.(*flags.Error); ok && fe.Type == flags.ErrHelp {
+			os.Exit(0)
+		}
+		log.Fatal(err)
+	}
+
+	readers := make([]io.Reader, 0, len(args)+1)
+	if len(args) == 0 {
+		readers = append(readers, os.Stdin)
+	}
+	for _, arg := range flag.Args() {
+		f, err := os.Open(arg)
+		if err != nil {
+			log.Fatalf("fail to open file %s: %s", arg, err)
+		}
+		defer f.Close()
+		readers = append(readers, f)
+	}
+
+	var sum float64
+	vals := make([]float64, 0, 1024*1024)
+
+	scanner := bufio.NewScanner(io.MultiReader(readers...))
+	for scanner.Scan() {
+		text := strings.TrimSpace(scanner.Text())
+		if val, err := strconv.ParseFloat(text, 64); err == nil {
+			sum += val
+			vals = append(vals, val)
+		}
+	}
+	if scanner.Err() != nil {
+		log.Fatalln(scanner.Err())
+	}
+
+	sort.Float64s(vals)
+
+	min := vals[0]
+	max := vals[len(vals)-1]
+	w := (max - min) / float64(opts.Bins)
+
+	var mcount int
+	bins := make([]int, opts.Bins, opts.Bins)
+
+	for _, val := range vals {
+		var idx int
+		if val != max {
+			idx = int((val - min) / w)
+		} else {
+			idx = opts.Bins - 1
+		}
+
+		bins[idx]++
+		if bins[idx] > mcount {
+			mcount = bins[idx]
+		}
+	}
+
+	fmt.Printf("Total count = %d\n", len(vals))
+	fmt.Printf("Min/Avg/Max = %.2f / %.2f / %.2f\n\n", min, w/float64(len(vals)), max)
+
+	for idx, count := range bins {
+		bmin := fmt.Sprintf("%8.2f", min+w*float64(idx))
+		bmax := fmt.Sprintf("%8.2f", min+w*float64(idx)+w)
+		bar := "  " + strings.Repeat("|", 40*count/mcount)
+
+		fmt.Printf("[%s - %s]\t%8d", bmin, bmax, count)
+		if opts.Chart {
+			fmt.Print(bar)
+		}
+		fmt.Println("")
+	}
+}
