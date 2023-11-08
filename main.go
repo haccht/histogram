6b0c23b1a87fccc2ab7026ee3526e4c36a43be45
haccht <haccht@users.noreply.github.com>
2023-11-08 12:43:23 +0000

    Refactor

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-    "text/tabwriter"
+	"text/tabwriter"
 
 	flags "github.com/jessevdk/go-flags"
 	"golang.org/x/crypto/ssh/terminal"
@@ -20,21 +20,21 @@ type options struct {
 	Chart bool `short:"c" long:"chart" description:"Draw the bar chart"`
 }
 
-func main() {
+func run() {
 	var opts options
 	args, err := flags.Parse(&opts)
 	if err != nil {
 		if fe, ok := err.(*flags.Error); ok && fe.Type == flags.ErrHelp {
 			os.Exit(0)
 		}
-		log.Fatal(err)
+		return err
 	}
 
 	readers := make([]io.Reader, 0, len(args)+1)
 	for _, arg := range args {
 		f, err := os.Open(arg)
 		if err != nil {
-			log.Fatalf("failed to open file %s: %s", arg, err)
+			return fmt.Errorf("failed to open file %s: %s", arg, err)
 		}
 		defer f.Close()
 		readers = append(readers, f)
@@ -58,7 +58,7 @@ func main() {
 		}
 	}
 	if scanner.Err() != nil {
-		log.Fatalln(scanner.Err())
+		return scanner.Err()
 	}
 
 	sort.Float64s(vals)
@@ -87,7 +87,7 @@ func main() {
 	fmt.Printf("Total count = %d\n", len(vals))
 	fmt.Printf("Min/Avg/Max = %.2f / %.2f / %.2f\n\n", min, w/float64(len(vals)), max)
 
-    tw := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight)
+	tw := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight)
 	for idx, count := range bins {
 		bmin := fmt.Sprintf("%8.2f", min+w*float64(idx))
 		bmax := fmt.Sprintf("%8.2f", min+w*float64(idx)+w)
@@ -99,5 +99,14 @@ func main() {
 		}
 		fmt.Fprintln(tw, "")
 	}
-    tw.Flush()
+	tw.Flush()
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stdout, err.Error())
+		os.Exit(1)
+	}
 }
